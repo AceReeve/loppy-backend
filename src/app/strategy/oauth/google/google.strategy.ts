b9903813c0c2b11edcda0c4ed1d11666792cf2d5
jake viovicente <jake@gowithhero.com>
2024-03-19 22:05:02 +0800

    convert google token to jwt token

--- src/app/strategy/oauth/google/google.strategy.ts
+++ src/app/strategy/oauth/google/google.strategy.ts
@@ -21,7 +21,13 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       scope: ['email', 'profile'],
     });
   }
-
+  //generate jwt token
+  generateJWT(payload: object, exp: string): string {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_EXPIRATION')
+    });
+  }
   async validate(
     accessToken: string,
     refreshToken: string,
@@ -29,12 +35,16 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ): Promise<any> {
     const { name, emails, photos } = profile;
+    //use jwt token instead of token provided by goolge
+    const payload = { email: emails[0].value };
+    const access_token = this.generateJWT(payload, this.configService.get<string>('JWT_EXPIRATION'));
+    //----//
     const user = {
       email: emails[0].value,
       firstName: name.givenName,
       lastName: name.familyName,
       picture: photos[0].value,
-      accessToken,
+      accessToken: access_token,
     };
     done(null, user);
     await this.oauthRepository.recordLogin(user, SignInBy.SIGN_IN_BY_GOOGLE);
