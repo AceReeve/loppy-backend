edfb5df2137047ec77577a09dff2a51cd468a712
jakeviovicente <jakev.zeniark@gmail.com>
2024-07-26 23:56:01 +0800

    fix role

--- src/app/models/invited-users/invited-users.schema.ts
+++ src/app/models/invited-users/invited-users.schema.ts
@@ -11,8 +11,8 @@ export type InvitedUserDocument = InvitedUser & Document;
 export class InvitedUser implements GenericSchema {
   _id: string;
 
-  @Prop([{ email: String, role: String, status: String }])
-  emails: { email: string; role: string; status: string }[];
+  @Prop([{ email: String, role: { _id: MongooseSchema.Types.ObjectId, role_name: String, description: String }, status: String }])
+  emails: { email: string; role: { _id: MongooseSchema.Types.ObjectId, role_name: string, description: string }; status: string }[];
 
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'User' })
   invited_by: MongooseSchema.Types.ObjectId;
